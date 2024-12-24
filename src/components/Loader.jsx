524bd9c82240417905b8c11d541fb70ce2f8b9e5
Maryam Esmaily <m.esmaily@digikala.com>
2024-12-24 12:30:53 +0330

    feat: add island image

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-20 h-20 border-2 border-blue-500/20 border-t-blue-500 rounded-full animate-spin" />
+    </div>
+  );
+};
+
+export default Loader;
