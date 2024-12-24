b5beb7706fa9f97b7adfe519d5a487f53739c7b7
Maryam Esmaily <m.esmaily@digikala.com>
2024-12-24 15:57:04 +0330

    feat: rotate sky and plane

--- src/components/models/Plane.jsx
+++ src/components/models/Plane.jsx
@@ -1,12 +1,24 @@
 /* eslint-disable react/no-unknown-property */
+import { useRef } from "react";
 import planeScene from "../../assets/3d/plane.glb";
-import { useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useEffect } from "react";
 
 const Plane = ({ isRotating, ...props }) => {
-  const { scene } = useGLTF(planeScene);
+  const { scene, animations } = useGLTF(planeScene);
+
+  const ref = useRef();
+
+  const { actions } = useAnimations(animations, ref);
+
+  useEffect(() => {
+    if (isRotating) {
+      actions["Take 001"].play();
+    } else actions["Take 001"].stop();
+  }, [actions, isRotating]);
 
   return (
-    <mesh {...props}>
+    <mesh {...props} ref={ref}>
       <primitive object={scene} />
     </mesh>
   );
--- src/components/models/Sky.jsx
+++ src/components/models/Sky.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable react/no-unknown-property */
+import { useRef } from "react";
 import skyScene from "../../assets/3d/sky.glb";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
-const Sky = () => {
+const Sky = ({ isRotating }) => {
+  const ref = useRef();
+
   const { scene } = useGLTF(skyScene);
 
+  useFrame((_, delta) => {
+    if (isRotating) {
+      ref.current.rotation.y += 0.15 * delta;
+    }
+  });
+
   return (
-    <mesh>
+    <mesh ref={ref}>
       <primitive object={scene} />
     </mesh>
   );
