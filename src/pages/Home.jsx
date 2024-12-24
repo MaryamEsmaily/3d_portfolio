f28e15d99285d3188cbb443ba66224a7bda2efb2
Maryam Esmaily <m.esmaily@digikala.com>
2024-12-24 15:19:44 +0330

    feat: rotate island

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,83 @@
+/* eslint-disable react/no-unknown-property */
 import { Canvas } from "@react-three/fiber";
 import Island from "../components/models/Island";
-
-{
-  /* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-  POPUP
-</div> */
-}
+import Sky from "../components/models/Sky";
+import Bird from "../components/models/Bird";
+import Plane from "../components/models/Plane";
+import { useState } from "react";
 
 const Home = () => {
+  const [currentStage, setCurrentStage] = useState(1);
+  const [isRotating, setIsRotating] = useState(false);
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition = [0, -6.5, -43];
-    let rotation = [0.1, 4.7, 0];
     if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
     } else {
       screenScale = [1, 1, 1];
     }
-    return [screenScale, screenPosition, rotation];
+    return [screenScale, screenPosition];
   };
 
-  const [screenScale, screenPosition, rotation] = adjustIslandForScreenSize();
+  const adjustPlaneForScreenSize = () => {
+    let planeScale, planePosition;
+    if (window.innerWidth < 768) {
+      planeScale = [1.5, 1.5, 1.5];
+      planePosition = [0, 0, -1];
+    } else {
+      planeScale = [2, 2, 2];
+      planePosition = [0, 0, -1];
+    }
+    return [planeScale, planePosition];
+  };
+
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && "<HomeInfo currentStage={currentStage} />"}
+      </div>
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         {/* <Suspense fallback={<Loader />}> */}
         {/* All kind of light in three.js */}
-        <directionalLight />
-        <ambientLight />
-        <pointLight />
-        <spotLight />
-        <hemisphereLight />
+        <directionalLight position={[1, 1, 1]} intensity={2} />
+        <ambientLight intensity={0.5} />
+        {/* <pointLight position={[10, 5, 10]} intensity={2} />
+        <spotLight
+          position={[0, 50, 10]}
+          angle={0.15}
+          penumbra={1}
+          intensity={2}
+        /> */}
+        <hemisphereLight
+          skyColor="#b1e1ff"
+          groundColor="#000000"
+          intensity={1}
+        />
+        <Bird />
+        <Sky isRotating={isRotating} />
         <Island
-          position={screenPosition}
-          scale={screenScale}
-          rotation={rotation}
+          isRotating={isRotating}
+          setIsRotating={setIsRotating}
+          setCurrentStage={setCurrentStage}
+          position={islandPosition}
+          rotation={[0.1, 4.7077, 0]}
+          scale={islandScale}
+        />
+        <Plane
+          isRotating={isRotating}
+          position={planePosition}
+          rotation={[0, 20.1, 0]}
+          scale={planeScale}
         />
         {/* </Suspense> */}
       </Canvas>
